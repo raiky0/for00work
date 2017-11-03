b4918f0d8d257dbbb38afef54c8d62d0f3f3757e
raiky <haibox.qi@intel.com>
2017-11-03 13:46:33 -0400

    tosql is simple successful

--- tosql.py
+++ tosql.py
@@ -12,26 +12,52 @@ class tosql():
 			'charset':'utf8'
 			}
 		self.con=pymysql.Connect(**config)
+		self.cs=self.con.cursor()
 	def insert(self,data,table):
 		col=list(data)
 		col_name=','.join(col)
-		print col_name
+		exists_table=self.table_if_exist(table)
+		if exists_table != (0,):self.create_table(table,col)
 		for i in range(len(data)):
 			value=list(data.iloc[i])
-			print value
-			value_name=','.join(map(str,value)) 
-			print value_name
-			try:
-				with self.con.cursor() as cs:
-					sql = 'insert into %s (%s) values (%s);' %(table,col_name,value_name)
-			#		sql = 'insert into w1 (age,name,sex) values (20,"ww","ww");'
-					opt=table,col_name,value_name
-					cs.execute(sql)
-					self.con.commit()
-			finally:
-	#			self.con.close()
-				pass
+			value_str=map(str,value)
+			value_name=','.join("'"+f+"'" for f in value_str)
+			for j in range(len(col)):
+				if j == 0:key_value=col[j] + '=' "'" + value_str[j] + "'"
+				else:key_value += ' and ' + col[j] + '=' "'" + value_str[j] + "'"
+			exist_yon=self.value_if_exist(key_value,table)
+			if exist_yon != (0,):
+				sql = 'insert into {0} ({1}) values ({2});'.format(table,col_name,value_name)
+				self.cs.execute(sql)
+				self.con.commit()
+				print "the data insert into %s successful" %(table)
+			else:print "the data is exist in %s" %(table)
 
+	def value_if_exist(self,arr,table):
+		sql='select 0 from {0} where {1};'.format(table,arr)
+		self.cs.execute(sql)
+		self.con.commit()
+		data = self.cs.fetchone()
+		return data
+
+	def table_if_exist(self,table):
+		sql='select 0 from information_schema.tables where table_name="{0}";'.format(table)
+		self.cs.execute(sql)
+		self.con.commit()
+		data = self.cs.fetchone()
+		return data
+		
+	def create_table(self,table,key):
+		for i in range(len(key)):
+			if i == 0:key_type=key[i] + ' TEXT '
+			else:key_type += ',' + key[i] + ' TEXT'
+		sql='create table {0} ({1})'.format(table,key_type)
+		self.cs.execute(sql)
+		self.con.commit()
+		print 'create table %s sucessful' %(table)
+		return self.con.commit()
+
+			
 if __name__=='__main__':
 	df=pd.DataFrame({'name':['raa','sssa','k'],'sex':['male','male','male'],'age':[1,3,5]})
 	ts=tosql('wc')
